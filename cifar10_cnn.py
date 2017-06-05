d0bd0c1615851d72208b5556bede865740607d7d
Carl Trimbach <ctrimbac@cs.brown.edu>
2017-06-05 16:31:27 -0400

    fixed cifar10 for keras 1.2.2

--- cifar10_cnn.py
+++ cifar10_cnn.py
@@ -25,8 +25,8 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # Convert class vectors to binary class matrices.
-y_train = keras.np_utils.to_categorical(y_train, num_classes)
-y_test = keras.np_utils.to_categorical(y_test, num_classes)
+y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 
