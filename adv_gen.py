1dfd82fc9e45a9c16b3df8e2c5176e493f094f1f
Carl Trimbach <ctrimbac@cs.brown.edu>
2017-06-22 15:27:40 -0400

    dropped dropout

--- adv_gen.py
+++ adv_gen.py
@@ -186,19 +186,19 @@ def cnn_model(in_shape, num_classes):
     model.add(Conv2D(32, 3, 3))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+#    model.add(Dropout(0.25))
 
     model.add(Conv2D(64, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
     model.add(Conv2D(64, 3, 3))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+#    model.add(Dropout(0.25))
 
     model.add(Flatten())
     model.add(Dense(512))
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+#    model.add(Dropout(0.5))
     model.add(Dense(num_classes))
     model.add(Activation('softmax'))
 
