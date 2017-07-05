c2fdd992441ee8e7f20b2ad4ccf8ddd40bbee1cc
Carl Trimbach <ctrimbac@cs.brown.edu>
2017-07-05 14:46:51 -0400

    syntax

--- adv_gen_own.py
+++ adv_gen_own.py
@@ -43,11 +43,11 @@ def main(argv=None):
         raise RuntimeError("This tutorial requires keras to be configured"
                            " to use the TensorFlow backend.")
 
-   # Image dimensions ordering should follow the Theano convention
-   if keras.backend.image_dim_ordering() != 'tf':
-       keras.backend.set_image_dim_ordering('tf')
-       print("INFO: '~/.keras/keras.json' sets 'image_dim_ordering' to "
-             "'th', temporarily setting to 'tf'")
+    # Image dimensions ordering should follow the Theano convention
+    if keras.backend.image_dim_ordering() != 'tf':
+        keras.backend.set_image_dim_ordering('tf')
+        print("INFO: '~/.keras/keras.json' sets 'image_dim_ordering' to "
+            "'th', temporarily setting to 'tf'")
 
     # Create TF session and set as Keras backend session
     sess = tf.Session()
