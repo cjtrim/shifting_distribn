7872e8ddf3344a507113284d1cf881a67cf8bc09
Carl Trimbach <ctrimbac@cs.brown.edu>
2017-07-20 14:58:07 -0400

    updated img display

--- adv_gen_2.py
+++ adv_gen_2.py
@@ -154,7 +154,7 @@ def main(argv=None):
     X_test_adv, = batch_eval(sess, [x], [adv_x], [X_test], args=eval_params)
     assert X_test_adv.shape[0] == 2000
 
-    X_test_adv = X_test.astype('float32')
+    X_test_adv = X_test_adv.astype('float32')
     img_adv = to_image(X_test_adv[1])
     img_adv.show()
     img_adv = to_image(X_test[1])
